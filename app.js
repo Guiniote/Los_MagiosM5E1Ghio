3e67b91c8f50fd0ddb72daa0f3e3fe2af381a745
Guiniote <guiniote@gmail.com>
2021-04-19 18:55:04 -0300

    Subo primera parte de controllers y router

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const app = express();
+
 const path = require('path');
 
-const app = express();
 const puerto = process.env.PORT;
+
+const homeRouter = require('./routes/homeRouter');
+const usersRouter = require('./routes/usersRouter');
+const productRouter = require('./routes/productRouter');
+
 
 app.use(express.static(path.join(__dirname, '/public')))
 
 app.set ('view engine', 'ejs');
 
+
+app.use ('/', homeRouter);
+//app.use ('/', usersRouter);
+//app.use ('/', productRouter);
+
+
+/*
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -23,6 +36,7 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login');
 });
+*/
 
 app.listen (puerto || 3000, () => {
     console.log('Servidor levantado en el puerto 3000');
